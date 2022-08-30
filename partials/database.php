f2a72f256134a7fffe21b6e2d707f569bbf682e6
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-08-30 08:56:24 +0500

    database connection

---
+++ partials/database.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "attendance_db";
+
+    $conn = mysqli_connect($servername,$username,$password,$database);
+    if(!$conn){
+        echo "Your database does not connected <br>";
+    }else{
+        // echo "Successfully your database connected <br>";
+    }
+
+?>

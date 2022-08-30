305557e997417bc8ca21d9410156a84294d2e3fe
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-08-30 08:57:15 +0500

    Handle login system

---
+++ partials/handle_login.php
@@ -0,0 +1,27 @@
+<?php
+        include_once("database.php");
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $sql = "SELECT * FROM `faculty` WHERE email = '$email';";
+
+            $result = mysqli_query($conn,$sql);
+            $row = mysqli_num_rows($result);
+            if($row > 0){
+                $data = mysqli_fetch_assoc($result);                
+                if($password == $data['password']){
+                    session_start();
+                    $_SESSION['email'] = $email;
+                    header("Location: ../index.php?login=true");
+                }else{
+                    header("Location: ../index.php?pass=false");
+                }
+            }else{
+                header("Location: ../signup.php?login=false");
+            }
+
+        }else{
+            echo "some tecnical issue";
+        }  
+?>

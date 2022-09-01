ba9fb40fcd448d7242d0e8c8786496bc8ed5acd0
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-01 23:49:44 +0500

    Footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,4 @@
+
 <div class="container-fluid bg-dark text-white m-0 p-0 text-center">
-    <p class="m-0 p-2">Copyright Attendance Management System 2022 | All right reserved</p>
+    <p class="m-0 p-2">Copyright Attendance Management System <?php echo date("Y"); ?> | All right reserved</p>
 </div>

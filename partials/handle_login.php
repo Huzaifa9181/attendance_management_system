18baf1882cc430c9f303f839d4da134ec7fcc707
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-05 03:41:38 +0500

    Handle login functionality

--- partials/handle_login.php
+++ partials/handle_login.php
@@ -4,15 +4,22 @@
             $email = $_POST['email'];
             $password = $_POST['password'];
 
-            $sql = "SELECT * FROM `faculty` WHERE email = '$email';";
+            $sql = "SELECT * FROM `users` WHERE email = '$email';";
 
             $result = mysqli_query($conn,$sql);
             $row = mysqli_num_rows($result);
             if($row > 0){
-                $data = mysqli_fetch_assoc($result);                
+                $data = mysqli_fetch_assoc($result); 
+                $role = $data['role'];        
+                $name = $data['name'];
+                $user = $data['Username'];
+
                 if($password == $data['password']){
                     session_start();
                     $_SESSION['email'] = $email;
+                    $_SESSION['role'] = $role;
+                    $_SESSION['name'] = $name;
+                    $_SESSION['username'] = $user;
                     $_SESSION['f_id'] = $data['id'];
                     $_SESSION['loggedin'] = "true";
                     header("Location: ../dashboard.php?login=true");

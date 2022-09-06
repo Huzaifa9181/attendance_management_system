8b6a2e04ee01f156a68fa964ed68f44975b4d03f
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-09-06 20:12:15 +0500

    database

--- attendance_db.sql
+++ attendance_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 02, 2022 at 10:17 PM
+-- Generation Time: Sep 06, 2022 at 12:19 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,27 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `faculty`
+-- Table structure for table `homework`
 --
 
-CREATE TABLE `faculty` (
+CREATE TABLE `homework` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` text NOT NULL,
-  `qualification` varchar(30) NOT NULL,
-  `Username` varchar(25) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `time` datetime NOT NULL DEFAULT current_timestamp()
+  `title` varchar(80) NOT NULL,
+  `description` text NOT NULL,
+  `faculty` varchar(50) NOT NULL,
+  `time` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `faculty`
+-- Dumping data for table `homework`
 --
 
-INSERT INTO `faculty` (`id`, `name`, `email`, `qualification`, `Username`, `user_id`, `password`, `time`) VALUES
-(1, 'huzaifa', 'huzaifa@gmail.com', 'intermediate', 'huzaifa11', 123, '123', '2022-08-30 08:19:46'),
-(2, 'admin', 'admin@gmail.com', 'asdadasd', 'admin', 123, '123', '2022-08-30 08:24:34');
+INSERT INTO `homework` (`id`, `title`, `description`, `faculty`, `time`) VALUES
+(1, 'Chapter 1 math test on tuesday', 'all student come with your test copy and learn this chapter carefully', '4', '2022-09-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'teacher'),
+(3, 'student');
 
 -- --------------------------------------------------------
 
@@ -69,20 +85,77 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `name`, `roll_no`, `course`, `semester`, `branch`, `attendance`, `f_id`, `time`) VALUES
-(5, 'John', 12, '123', '131', 'sdasdsa', 'present', 2, '2022-09-01'),
-(7, 'Huzaifa', 13, 'BS (Computer Sciences)', '4th Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'absent', 2, '2022-09-01'),
-(8, 'Abdullah', 112, 'BS (Computer Sciences)', '6th Semester', 'Gulshan Campus - Abid Town, Block-2, Gulshan-e-Iqbal.', 'present', 2, '2022-09-01'),
-(9, 'Hamza', 222, 'BE Software Engineering.', '8th Semester', 'Bahria Town Campus - Bahria town.', 'absent', 2, '2022-09-01'),
-(10, 'Huzaifa', 13, 'BS (Computer Sciences)', '4th Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 2, '2022-09-02');
+(14, 'Huzaifa', 1, 'BS (Computer Sciences)', '1st Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-01'),
+(15, 'Huzaifa', 1, 'BS (Computer Sciences)', '1st Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-02'),
+(16, 'Huzaifa', 1, 'BS (Computer Sciences)', '1st Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-03'),
+(17, 'Huzaifa', 1, 'BS (Computer Sciences)', '1st Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'absent', 4, '2022-09-04'),
+(18, 'Abdullah', 2, 'BE Software Engineering.', '3rd Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-04'),
+(22, 'Abdullah', 2, 'BE Software Engineering.', '3rd Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'absent', 4, '2022-09-04'),
+(24, 'Huzaifa', 1, 'BS (Computer Sciences)', '1st Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-05'),
+(25, 'Abdullah', 2, 'BE Software Engineering.', '3rd Semester', 'Main Campus - Defence view, Shaheed-e-Millat Road.', 'present', 4, '2022-09-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submit_homework`
+--
+
+CREATE TABLE `submit_homework` (
+  `id` int(11) NOT NULL,
+  `file_path` varchar(90) NOT NULL,
+  `f_id` int(11) NOT NULL,
+  `std_name` varchar(50) NOT NULL,
+  `time` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `submit_homework`
+--
+
+INSERT INTO `submit_homework` (`id`, `file_path`, `f_id`, `std_name`, `time`) VALUES
+(1, 'js.jfif', 4, 'Abdullah', '2022-09-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` text NOT NULL,
+  `qualification` varchar(30) NOT NULL,
+  `Username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `qualification`, `Username`, `password`, `role`, `time`) VALUES
+(4, 'Sami', 'sami@gmail.com', 'bachelors', 'sami12', '123', '2', '2022-09-04 15:17:32'),
+(5, 'Huzaifa', 'huzaifa@gmail.com', 'bachelors', 'Huzaifa', '123', '3', '2022-09-04 16:11:12'),
+(6, 'Abdullah', 'abdullah@gmail.com', 'intermediate', 'Abdullah11', '123', '3', '2022-09-04 18:20:40'),
+(7, 'Hamza', 'hamza@gmail.com', 'bachelors', 'Hamza', '123', '2', '2022-09-05 03:57:47');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `faculty`
+-- Indexes for table `homework`
 --
-ALTER TABLE `faculty`
+ALTER TABLE `homework`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -92,20 +165,50 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `submit_homework`
+--
+ALTER TABLE `submit_homework`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `faculty`
+-- AUTO_INCREMENT for table `homework`
 --
-ALTER TABLE `faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `homework`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `submit_homework`
+--
+ALTER TABLE `submit_homework`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
